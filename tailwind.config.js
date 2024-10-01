c4e62c1d9cf0fceed438f2ae67cbda90a6c73796
Karan <email@karanshergill.com>
2024-10-02 01:41:26 +0530

    add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'main': "hsl(217, 28%, 15%)",
+          'intro': "hsl(218, 28%, 13%)",
+          'signup': "hsl(218, 28%, 13%)",
+          'footer': "hsl(216, 53%, 9%)",
+          'testimonials': "hsl(219, 30%, 18%)"
+        },
+        accent: {
+          'cyan': "hsl(176, 68%, 64%)",
+          'blue': "hsl(198, 60%, 50%)",
+          'red': "hsl(0, 100%, 63%)"
+        }
+      },
+      fontFamily: {
+        'raleway': ["'Raleway Variable', 'sans-serif'"]
+      }
+    },
+    screens: {
+      'mobile': "375px",
+      'desktop': "1440px"
+    }
   },
   plugins: [],
 }
